a3f37716d161579bec99e9b011c2db5228264a05
arkinem <blazejgolinski@live.com>
2020-04-23 10:33:49 +0100

    fixes typo

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -1,6 +1,6 @@
 export default {
   error: {
-    invalidPlateauSize: "Ivalid plateau size",
+    invalidPlateauSize: "Invalid plateau size",
     invalidRoverPositionLine: "Invalid rover position line [index?]",
     initialPositionOutsidePlateau: "Rover initial position is outside plateau",
     invalidInstructionsLine: "Invalid instructions line [index?]",
