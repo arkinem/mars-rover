3f0a5833eed269574ec6bf85c7ec59744a574d3c
arkinem <blazejgolinski@live.com>
2020-04-27 10:34:44 +0100

    adds test helpers

---
+++ src/__tests__/helpers/testHelpers.ts
@@ -0,0 +1,19 @@
+import { RoverLocation, Heading, Rover } from "../../helpers/rover";
+
+export const createLocation = (
+  x: number,
+  y: number,
+  heading?: Heading
+): RoverLocation => ({
+  x: x,
+  y: y,
+  heading: heading || Heading.North,
+});
+
+export const createRover = (path?: RoverLocation[]): Rover => ({
+  initialLocation:
+    path && path.length > 0 ? { ...path[0] } : createLocation(0, 0),
+  instructions: "",
+  path: path ? path : [createLocation(0, 0), createLocation(0, 1)],
+  error: null,
+});
